006bd55c7f616adcffe79b135e6c2fdf11e8526d
Joseun <ologanj@gmail.com>
2021-12-04 16:09:01 +0000

    fixed the getline error

--- monty.c
+++ monty.c
@@ -1,21 +1,16 @@
+#define _GNU_SOURCE
 #include "monty.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 
 int main(int ac, char **av)
 {
 	char *line = NULL;
 	FILE *monty_file;
 	size_t n = 0;
-	int line_no = 0;
+	ssize_t line_no;
 
 	check_args(ac);
-	monty_file = open_file(av[1], "r");
-	while (getline(&line, &n, monty_file) != -1)
+	monty_file = fopen(av[1], "r");
+	while ((line_no = getline(&line, &n, monty_file)) != -1)
 	{
 		line_no++;
 		evaluate(line, line_no);
