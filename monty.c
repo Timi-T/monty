d0173176c31378758062ca0b873dcb0763dc8702
Timi-T <timiopebode@gmail.com>
2021-12-04 16:02:50 +0000

    update

--- monty.c
+++ monty.c
@@ -1,10 +1,4 @@
 #include "monty.h"
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
 
 int main(int ac, char **av)
 {
--- monty.h
+++ monty.h
@@ -1,7 +1,15 @@
 #ifndef STACK_H
 #define STACK_H
 
+#define _GNU_SOURCE
+#include "monty.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
 /**
 * struct stack_s - doubly linked list representation of a stack (or queue)
